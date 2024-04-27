262cad4fcb0fc520c710d854e79ea2480778d0cd
Максим Пьянков <pya4kov@gmail.com>
2024-04-27 13:45:03 +0500

    v1.3.4

--- services/controller/cmd/main.go
+++ services/controller/cmd/main.go
@@ -1,43 +1,36 @@
 package main
 
 import (
-	"context"
-	"log"
-	"net"
-
-	pb "hack/services/goserv/gen"
-
-	"google.golang.org/grpc"
+	. "hack/services/controller/internal/cli"
+	cfg "hack/services/controller/pkg/handleconfig"
 )
 
-type server struct {
-	pb.UnimplementedStringServiceServer
-}
+// "context"
+// "log"
+
+// pb "hack/services/controller/gen"
+
+// "google.golang.org/grpc"
 
 func main() {
-	lis, err := net.Listen("tcp", ":50051")
-	if err != nil {
-		log.Fatalf("failed to listen %v", err)
-	}
-	s := grpc.NewServer()
-	pb.RegisterStringServiceServer(s, &server{})
-	log.Printf("server listening at %v", lis.Addr())
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
-}
+	action, service, port := RunCLI()
+	cfg.ChangeJson(action, service, port)
 
-func (s *server) SendString(ctx context.Context, in *pb.StringRequest) (*pb.StringReply, error) {
-	log.Printf("Received message: %s", in.GetMessage())
+	// conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	// if err != nil {
+	// 	log.Fatalf("Failed to connection: %v", err)
+	// }
+	// defer conn.Close()
+	// client := pb.NewSumServiceClient(conn)
+	// stream, err := client.Sum(context.Background())
+	// if err != nil {
+	// 	log.Fatalf("%v.Sum(_) = _, %v", client, err)
+	// }
 
-	if in.GetMessage() == "к" {
-		return &pb.StringReply{Message: "жопа"}, nil
-	}
-
-	return &pb.StringReply{Message: "Message for client : " + in.GetMessage()}, nil
 }
 
 // TODO:
 // 1. Сделать мониторинг работы микросервисов-моков по эндпоинту /status, мы можем добавлять в обработку, мониторим карту запущенных микросервисов ( id:host )
 // сервиса контроллера новые микросервисы ( выстроить работу с конфигом JSON ) тобешь добавляем в json файл конфигурации новый порт или хост. ( микросервисы должны быть заранее созданы и запущены)
 // 2. Сделать возможность запуска микросервисов через cli
+// 3. Добавить поддержку стримов
